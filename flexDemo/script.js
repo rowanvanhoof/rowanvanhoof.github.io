0ae5904bae744f5241d470c806ea90a9a48f7455
rowanvanhoof <52942145+rowanvanhoof@users.noreply.github.com>
2021-03-08 22:02:16 -0500

    Update script.js

--- flexDemo/script.js
+++ flexDemo/script.js
@@ -5,32 +5,32 @@ const switch2Elements = ["row1", "row2", "row3"];
 const switch2CodeBlock = ["rowDispBlock", "rowDispFlex"];
 
 
-switchControls("#switch1", switch1Elements, switch1CodeBlock);
-
-switchControls("#switch2", switch2Elements, switch2CodeBlock);
-
+switchControls("switch1", switch1Elements, switch1CodeBlock);
+switchControls("switch2", switch2Elements, switch2CodeBlock);
 
 function switchControls (controlId, elementId, codeBlockId) {
-	const cb = document.querySelector(controlId);
-    var count1 = 0;
+	const controlIdAlt = "#" + controlId;
+	const cb = document.querySelector(controlIdAlt);
+   
+    var state = cb.dataset.switchState;
+    cb.setAttribute(state, "off" );
     
     cb.onclick = () => {
 		const result = cb.value;
-		count1++;
+		var switch1 = document.getElementById(controlId);
 		
-		for(var i = 0; i < elementId.length; i++){
-			if(count1 % 2 == 1){
+		if(switch1.getAttribute(state) == "off"){
+			for (var i = 0; i < elementId.length; i++) {
 				document.getElementById(elementId[i]).style.display = "flex";
-
-				document.getElementById(codeBlockId[0]).style.visibility = "hidden";
-				document.getElementById(codeBlockId[1]).style.visibility = "visible";
-			}else {
-				document.getElementById(elementId[i]).style.display = "block";
-				
-				document.getElementById(codeBlockId[1]).style.visibility = "hidden";
-				document.getElementById(codeBlockId[0]).style.visibility = "visible";
-
+				switch1.setAttribute(state, "on" );
 			}
-		}	
+		}else {
+			for (var i = 0; i < elementId.length; i++) {
+				document.getElementById(elementId[i]).style.display = "block";
+				switch1.setAttribute(state, "off" );
+			}
+		}
+
+		
 	}
 }
